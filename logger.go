bf60dbfe22dceaa94918a9c6d0d77a4924c78305
abdallaEG <52584554+alanEG@users.noreply.github.com>
2022-08-27 15:41:46 -0700

    Update logger.go

--- logger.go
+++ logger.go
@@ -21,9 +21,14 @@ func logger(typeLog, Flag, message, reason interface{}, errorRange int) {
 	case "success":
 		fmt.Fprintf(os.Stderr, "%s %s\n", yellow(Flag), message)
 	case "error":
-		if reason != nil {
+		if reason != "" {
 			fmt.Fprintf(os.Stderr, "%s %s\n", red("[Error]"), message)
 			fmt.Fprintf(os.Stderr, "Reason: %s\n", reason)
+		} else {
+			fmt.Fprintf(os.Stderr, "%s %s\n", red("[Error]"), message)
+		}
+		if errorRange == 1 {
+			os.Exit(0)
 			if errorRange == 1 {
 				os.Exit(0)
 			}
